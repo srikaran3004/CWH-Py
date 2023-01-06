5701c9a1a49df70fbe8732751feabba4b76ed9a3
Srikaran <srikaran2230@gmail.com>
2023-01-06 10:32:43 +0530

    Python learning

---
+++ comments,escape-sequence,print.py
@@ -0,0 +1,17 @@
+print("Hey Iam a good boy \nand this viewer is a also a good boy/girl")
+# \n make the next text in the next line 
+# for comments we use # symbol (ctrl+/)
+'''
+Triple single and double code helps in other way of using #
+'''
+# \n is called the escape sequence charecter 
+
+# print("This doesnt "execute")
+# print("This will \" execute")
+# if we want to use double quote in a single line for multiple times then apply forward slash and rum it will apply double quotes for the text we want 
+
+# An escape sequence character is a backslash \ followed by the character you want to insert.
+
+print("My","name","is","Srikaran",sep=".")
+# by using sep="the item with which we want to seperate" we can get the words seperated by the sep text 
+# file: An object with a write method. Default is sys.stdout
